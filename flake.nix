7946fc9ef2ad422481104b1bf9ca47c585cf6baa
Michael Glass <me@mike.is>
2022-10-17 17:38:14 +0200

    pull in newer ruby from nix and ignore gems

---
+++ flake.nix
@@ -0,0 +1,28 @@
+# this file installs a reasonable development environment
+# see https://gist.github.com/michaelglass/6d1af2cc869268fa7a17820b72ca7e48
+# for simple nix setup instructions
+{
+  description = "captain dev environment";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { system = system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          nixfmt # formatting _this file!_
+          ruby_3_1 # locked to 3.1.2p20, with bundler 2.3.19
+          git # locked to 2.36.2
+        ];
+      };
+    });
+}
