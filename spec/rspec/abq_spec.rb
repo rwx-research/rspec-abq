18795d27f39bd7a1203a38eb7319e55ae32c4bf9
Michael Glass <fork@mike.is>
2022-10-18 14:45:32 +0200

    resolve rubocop

--- spec/rspec/abq_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Rspec::Abq do
-  it "has a version number" do
-    expect(Rspec::Abq::VERSION).not_to be_nil
-  end
-
-  it "does something useful" do
-    expect(false).to be(true)
-  end
-end
