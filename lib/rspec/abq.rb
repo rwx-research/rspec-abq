84703795545e4acae58d0f4ab0fb74c28db3b217
Michael Glass <me@mike.is>
2022-10-20 13:05:01 +0200

    add a bunch of comments

--- lib/rspec/abq.rb
+++ lib/rspec/abq.rb
@@ -12,6 +12,8 @@ require_relative "abq/version"
 module RSpec
   # An abq adapter for RSpec!
   module Abq
+    # the socket used to communicate to the abq worker
+    # looks like "ip.address.3.4:port" e.g. "0.0.0.0:1234"
     # @visibility private
     ABQ_SOCKET = "ABQ_SOCKET"
 
@@ -21,10 +23,12 @@ module RSpec
 
     # this is set by the outer-most rspec runner to ensure nested rspecs aren't ABQ aware.
     # if we ever want nested ABQ rspec, we'll need to change this.
+    # this env var is unrelated to the abq worker!
+    # @visibility private
     ABQ_RSPEC_PID = "ABQ_RSPEC_PID"
 
     # The [ABQ protocol version message](https://www.notion.so/rwx/ABQ-Worker-Native-Test-Runner-IPC-Interface-0959f5a9144741d798ac122566a3d887#8587ee4fd01e41ec880dcbe212562172).
-    # Must be sent to ABQ_SOCKET on startup, if running in ABQ mode.
+    # Must be sent to ABQ_SOCKET on startup.
     # @visibility private
     PROTOCOL_VERSION_MESSAGE = {
       type: "abq_protocol_version",
@@ -32,13 +36,21 @@ module RSpec
       minor: 1
     }
 
+    # The [ABQ initialization success
+    # message](https://www.notion.so/rwx/ABQ-Worker-Native-Test-Runner-IPC-Interface-0959f5a9144741d798ac122566a3d887#538582a3049f4934a5cb563d815c1247)
+    # Must be sent after receiving the ABQ initialization message.
+    # @visibility private
     INIT_SUCCESS_MESSAGE = {}
 
     # Whether this rspec process is running in ABQ mode.
+    # @return [Boolean]
     def self.enabled?(env = ENV)
       env.key?(ABQ_SOCKET) && (!env.key?(ABQ_RSPEC_PID) || env[ABQ_RSPEC_PID] == Process.pid.to_s)
     end
 
+    # This is the main entry point for abq-rspec, and it's called when the gem is loaded
+    # @visisbility private
+    # @return [void]
     def self.setup!
       return unless enabled?
       Extensions.setup!
@@ -51,7 +63,12 @@ module RSpec
     def self.setup_after_specs_loaded!
       return if setup?
       ENV[ABQ_RSPEC_PID] = Process.pid.to_s
+      # ABQ doesn't support writing example status to disk yet.
+      # in its simple implementation, status persistance write the status of all tests which ends up hanging with under
+      # abq because we haven't run most of the tests in this worker. (maybe it's running the tests?). In any case:
+      # it's disabled.
       RSpec.configuration.example_status_persistence_file_path = nil
+
       # before abq can start workers, it asks for a manifest
       if !!ENV[ABQ_GENERATE_MANIFEST] # the abq worker will set this env var if it needs a manifest
         RSpec::Abq::Manifest.write_manifest(RSpec.world.ordered_example_groups, RSpec.configuration.seed, RSpec.configuration.ordering_registry)
@@ -62,9 +79,15 @@ module RSpec
         return
       end
 
-      RSpec.configuration.color_mode = :on
-      # this should disable persisting_example_statuses
+      # after the manfiest has been sent to the worker, the rspec process will quit and the workers will each start a
+      # new rspec process
 
+      # enabling colors allows us to pass through nicer error messages
+      RSpec.configuration.color_mode = :on
+
+      # the first message is the init_meta block of the manifest. This is used to share runtime configuration
+      # information amongst worker processes. In RSpec, it is used to ensure that random ordering between workers
+      # shares the same seed, so can be deterministic.
       message = protocol_read
       init_message = message["init_meta"]
       if init_message
@@ -72,6 +95,9 @@ module RSpec
         protocol_write(INIT_SUCCESS_MESSAGE)
         fetch_next_example
       else
+        # to support the old protocol, we don't depend on the initialization method, however we don't support random
+        # ordering via config, only via a shared command line seed. `abq test -- rspec --seed 4` will pass the
+        # deterministic seed to all workers.
         fetch_next_example(message)
       end
     end
@@ -79,7 +105,8 @@ module RSpec
     UnsupportedOrderingClassError = Class.new(StandardError)
     AbqInitFailed = Class.new(StandardError)
 
-    # the socket to communicate with ABQ worker
+    # Creates the socket to communicate with the worker and sends the worker the protocol
+    # @visibility private
     def self.socket
       @socket ||= TCPSocket.new(*ENV[ABQ_SOCKET].split(":")).tap do |socket|
         protocol_write(PROTOCOL_VERSION_MESSAGE, socket)
@@ -92,10 +119,15 @@ module RSpec
         ENV.key?("ABQ_DISABLE_TESTS") # just here to test this
     end
 
-    # used internally to split off tags from built in rspec metadata
+    # These are the metadata keys that rspec uses internally on examples and groups
+    # When we want to report custom tags that rspec-users write, we need to remove these from the example metadata
+    # @visibility private
     RESERVED_METADATA_KEYS = Set.new(RSpec::Core::Metadata::RESERVED_KEYS + [:if, :unless])
 
-    # extracts relevant metadata from an a group or example's metadata
+    # Takes group or example metadata and returns a two-element array:
+    # a tag is any piece of metadata that has a value of true
+    # @returns [Array<Array<Symbol>, Hash<Symbol, Object>>] tags and metadata
+    # @visibility private
     def self.extract_metadata_and_tags(metadata)
       # we use `.dup.reject! because `.reject` raises a warning (because it doesn't dup procs)`
       user_metadata = metadata.dup.reject! { |k, _v| RESERVED_METADATA_KEYS.include?(k) }
@@ -104,10 +136,13 @@ module RSpec
     end
 
     class << self
+      # the target_test_case is the test case the abq worker wants results for
+      # @visibility private
       attr_reader :target_test_case
     end
 
-    # pulls next example from abq and sets it to #target_test_case
+    # pulls next example from the abq worker and sets it to #target_test_case
+    # @visibility private
     def self.fetch_next_example(message = protocol_read)
       @target_test_case =
         if message == :abq_done
