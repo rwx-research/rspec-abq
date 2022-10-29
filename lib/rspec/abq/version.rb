f970d73f6d5c4da2728378b4c2f17e15693c6279
Michael Glass <me@mike.is>
2022-10-29 14:17:33 +0200

    prepare release v (#28)
    * Bump rspec-abq to 0.2.0
    
    * fix small bug in release script

--- lib/rspec/abq/version.rb
+++ lib/rspec/abq/version.rb
@@ -1,6 +1,6 @@
 module RSpec
   module Abq
     # current version!
-    VERSION = "0.1.0"
+    VERSION = "0.2.0"
   end
 end
--- release_gem.rb
+++ release_gem.rb
@@ -95,7 +95,7 @@ if version_to_release == latest_released_version
   run_and_print("git add Gemfile.lock")
   run_and_print("git commit --amend --no-edit")
   run_and_print("git push origin HEAD")
-  run_and_print('gh pr create --title "prepare release v$next_version" --fill')
+  run_and_print(%(gh pr create --title "prepare release v#{next_version}" --fill))
   puts "Now: merge a PR with this version and then continue releasing the gem from the main branch."
   exit(0)
 end
