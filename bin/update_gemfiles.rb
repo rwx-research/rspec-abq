53cee355d8875f3bff9ce3bd90730f05339b2103
Michael Glass <me@mike.is>
2022-10-31 22:23:41 +0100

    add linux platform to all test lockfiles

--- bin/update_gemfiles.rb
+++ bin/update_gemfiles.rb
@@ -5,5 +5,5 @@ GEMFILES = Dir['gemfiles/*.gemfile'] + ["Gemfile"]
 GEMFILES.each do |gemfile|
   ENV['BUNDLE_GEMFILE'] = gemfile
   puts gemfile
-  puts `bundle update`
+  puts `bundle lock --add-platform x86_64-linux`
 end
