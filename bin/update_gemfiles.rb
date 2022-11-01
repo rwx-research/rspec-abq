2182dbd55ae7b4e827d3a543ec85b9ebc43673fc
Michael Glass <me@mike.is>
2022-11-01 08:48:50 +0100

    whoops! fix bin/update_gemfiles

--- bin/update_gemfiles.rb
+++ bin/update_gemfiles.rb
@@ -5,6 +5,6 @@ GEMFILES = Dir['gemfiles/*.gemfile'] + ["Gemfile"]
 GEMFILES.map do |gemfile|
   Thread.new do
     ENV['BUNDLE_GEMFILE'] = gemfile
-    puts `bundle lock --add-platform x86_64-linux`
+    puts `bundle install`
   end
 end.map(&:join)
