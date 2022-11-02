6030e83ba86174e4565e7b32e91b0cb55fbfc7bd
Michael Glass <me@mike.is>
2022-11-02 16:06:16 +0100

    add regression test
    does this fix regression test?
    
    how about this
    
    does this work?
    
    modify test generator a little
    
    still trying to debug test here
    
    worker now fails -- ignore the failure
    
    clean up the script
    
    add a comment
    
    fix the test command
    
    use tee
    
    newlines aren't happy?
    
    maybe maybe maybe
    
    use more permissive bash
    
    does this work too?

--- spec/fixture_specs/one_specs.rb
+++ spec/fixture_specs/one_specs.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 # called `_specs.rb` to avoid it being called automatically
 # used by integration/abq_spec.rb
 RSpec.describe 'group 1' do
-  it 'example 1 with tags', :foo, :bar do end
+  it 'example 1 with tags', :foo, :bar do
+    expect(false).to eq(true)
+  end
   it 'example 2 with tag and value', :foo, bar: 5 do end
   describe 'group 1-1 with tags', :foo  do
     xit 'pending group 1-1 example 1' do end
