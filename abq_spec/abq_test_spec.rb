d77733982ddd04f663d04b738c4828ad76b0b53d
Michael Glass <me@mike.is>
2022-11-24 20:32:21 +0100

    add success test to abq integration tests

--- abq_spec/abq_test_spec.rb
+++ abq_spec/abq_test_spec.rb
@@ -1,20 +1,40 @@
 require "open3"
-# require "pry"
 require "spec_helper"
 
-RSpec.describe "abq test" do
-  it "has consistent output for success"
+RSpec.describe "abq test" do # rubocop:disable RSpec/DescribeClass
+  def abq_with(rspec_command, identifier)
+    run_id = ENV["RUN_ID"] && "--run-id #{ENV["RUN_ID"]}-#{identifier} "
+    Open3.capture3("abq test --reporter dot #{run_id}-- #{rspec_command}")
+  end
 
-  it "has consistent output for failure" do
-    stdout, stderr, status = Open3.capture3("abq test --reporter dot -- bundle exec rspec --out /dev/null --pattern 'spec/fixture_specs/*_specs.rb'")
-    gemfile_location = ENV["BUNDLE_GEMFILE"]
-    test_identifier = "failure"
-    file_path = "abq_spec/test-outputs/#{test_identifier}-#{File.basename(gemfile_location)}.txt"
-    matchable_output = stdout.lines[0...-1].join # drop last line because it has timing information
+  def write_or_match(test_identifier, matchable_output)
+    file_path = "abq_spec/test-outputs/#{test_identifier}-#{File.basename(ENV["BUNDLE_GEMFILE"])}.txt"
     if File.exist?(file_path)
       expect(matchable_output).to eq(File.read(file_path))
     else
       File.write(file_path, matchable_output)
     end
+  end
+
+  def sanitize_output(output)
+    output.gsub(/completed in \d+ ms/, "completed in 0 ms")
+  end
+
+  def stderr_matcher(stderr)
+    expect(stderr.lines).to all(match(/warning: |(?:^Worker started)/))
+  end
+
+  it "has consistent output for success" do
+    stdout, stderr, status = abq_with("bundle exec rspec --out /dev/null 'spec/fixture_specs/two_specs.rb'", "success")
+    write_or_match("success", sanitize_output(stdout))
+    stderr_matcher(stderr)
+    expect(status.exitstatus).to eq(0)
+  end
+
+  it "has consistent output for failure" do
+    stdout, stderr, status = abq_with("bundle exec rspec --out /dev/null --pattern 'spec/fixture_specs/*_specs.rb'", "failure")
+    write_or_match("failure", sanitize_output(stdout))
+    stderr_matcher(stderr)
+    expect(status.exitstatus).to eq(1)
   end
 end
