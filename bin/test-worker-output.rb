b0b19c36a612054576ac8128103bbe2a0d368d64
Michael Glass <me@mike.is>
2022-11-24 21:52:27 +0100

    test worker exit code

--- bin/test-worker-output.rb
+++ bin/test-worker-output.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
-unless $stdin.stat.pipe?
-  warn 'Must be called via a pipe'
-  exit 1
-end
+require 'open3'
+success_or_failure = ARGV[0]
 
-output = $stdin.read
+run_id = "#{ENV['RUN_ID']}-#{success_or_failure}}"
+
+command = "abq work --run-id #{run_id} -n cpu-cores"
+puts "Running: #{command}"
+output, status = Open3.capture2(command)
 output.each_line do |line|
   next if line.start_with?("Worker started with id ") || line.include?(" warning: ") || line.match?(/INFO\s*\033\[0m\s*abq_workers/)
   warn "=================="
@@ -20,3 +22,6 @@ output.each_line do |line|
 end
 
 puts output
+
+should_succeed = success_or_failure  == "success"
+exit 1 if should_succeed != status.success?
