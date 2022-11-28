4ed75bfa9ff75a69293ad124b69e92ca77efc3fc
Michael Glass <me@mike.is>
2022-11-28 12:18:00 +0100

    cleanup now non-optional params

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -2,24 +2,14 @@ require "open3"
 require "spec_helper"
 
 RSpec.describe "abq test" do # rubocop:disable RSpec/DescribeClass
-  def abq_test(rspec_command, queue_addr: nil, run_id: nil)
-    args = ["abq", "test"]
-
-    if queue_addr
-      args << "--queue-addr" << queue_addr
-    end
-
-    if run_id
-      args << "--run-id" << run_id
-    end
-
-    args << "--" << rspec_command
-
-    command = args.join(" ")
+  def abq_test(rspec_command, queue_addr:, run_id:)
+    command = ["abq", "test", "--queue-addr", queue_addr, "--run-id", run_id, '--', rspec_command].join(" ")
     warn "Running: #{command}"
     Open3.capture3(command)
   end
 
+  # if test output doesn't exist on disk, write it to a file
+  # if it does exist, use the file as the expected output
   def write_or_match(test_identifier, matchable_output)
     file_path = "spec/test-outputs/#{test_identifier}-#{File.basename(ENV["BUNDLE_GEMFILE"])}.txt"
     if !File.exist?(file_path) || ENV["UPDATE_SNAPSHOTS"]
