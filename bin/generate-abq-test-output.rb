ff853e6895e0638d3ad4f895d3f775b012f61a5f
Michael Glass <me@mike.is>
2022-11-28 11:21:02 +0100

    symlink default gemfiles

--- bin/generate-abq-test-output.rb
+++ bin/generate-abq-test-output.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 Dir['gemfiles/*.gemfile'].map do |gemfile|
   Thread.new do
     puts gemfile
@@ -8,3 +10,13 @@ Dir['gemfiles/*.gemfile'].map do |gemfile|
     puts `bundle exec rspec abq_spec/abq_test_spec.rb`
   end
 end.map(&:join)
+
+# symlink the results for the latest gemfile to the results for the default gemfile
+
+Dir.chdir('abq_spec/test-outputs') do
+  Dir['*-rspec-3.12.gemfile.txt'].each do |source|
+    target = source.sub('rspec-3.12.gemfile', 'Gemfile')
+    File.rm(target) if File.exist? target
+    FileUtils.ln_s(source, target)
+  end
+end
