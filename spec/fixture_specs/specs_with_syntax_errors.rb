7d80f8d962d850111752de306c93aa25584002c2
Michael Glass <me@mike.is>
2022-12-07 19:58:42 +0100

    explain naming of syntax error  fixture

--- spec/fixture_specs/specs_with_syntax_errors.rb
+++ spec/fixture_specs/specs_with_syntax_errors.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
-# called `_specs.rb` to avoid it being called automatically
+# this file is named differently than all the other fixture_specs.rb so it doesn't get globbed with them
+# if it gets loaded, no specs run
 # used by feature specs
 RSpec.describe 'with syntax errors" do
   it is getting pretty wacky in here do
