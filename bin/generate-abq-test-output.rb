4ad61dd7637187bf73f48f25eca6988a6849c530
Michael Glass <me@mike.is>
2022-12-09 00:00:42 +0100

    check num tests vs expected tests

--- bin/generate-abq-test-output.rb
+++ bin/generate-abq-test-output.rb
@@ -19,12 +19,14 @@ end
 
 threads.map(&:join)
 
-
-ENV['UPDATE_SNAPSHOTS'] = 'true'
-`bundle exec rspec spec/features/manifest_spec.rb`
+# rspec-abq calls exit(0). Loading the code path that calls exit(0) returns from rspec immediately with a exit status of 0
+# which shows as "passing" in CI even if tests failed.
+#
+# Here, we count the tests without running them and save them in spec/NUM_TESTS.
+# In CI, we fail the build if the number of tests in the results JSON isn't equal to this number.
+`bundle exec rspec --dry-run --format json | jq '.examples | length' > spec/NUM_TESTS`
 
 # symlink the results for the latest gemfile to the results for the default gemfile
-
 Dir['spec/**/*-rspec-3.12.gemfile.snap'].each do |path|
   Dir.chdir(File.dirname(path)) do
     source = File.basename(path)
