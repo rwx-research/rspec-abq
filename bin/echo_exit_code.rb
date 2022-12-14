0e3527be9c9b4ae91770dab4831998e71467b757
Michael Glass <me@mike.is>
2022-12-14 11:39:29 +0100

    expand on this comment

--- bin/echo_exit_code.rb
+++ bin/echo_exit_code.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
 # abq uses test results to determine whether to exit with 0 or 1.
-# this echos the exit code for comparing with in tests
+# abq (as far as I know?) ignores the native runner's exit code.
+#
+# this echos the native runner's exit code to ensure it's what we want in the test code.
 
 system(*ARGV)
 
