3cfc69d0f6afb405f5584af9c106d28fc89ac4aa
Dan Manges <dan@rwx.com>
2023-01-24 09:00:12 -0500

    Fix a bug when an exception occurs in a before all block

--- spec/fixture_specs/raising_specs.rb
+++ spec/fixture_specs/raising_specs.rb
@@ -5,4 +5,20 @@ RSpec.describe 'a raising group' do
   it 'has a raising test' do
     raise 'the roof'
   end
+
+  context "it can handle an exception in a before all hook" do
+    before(:all) do
+      raise "raising from before(:all)"
+    end
+
+    it "should fail" do
+      # should fail without running because of the exception in the before(:all)
+      exit 9999
+    end
+
+    it "should also fail" do
+      # should fail without running because of the exception in the before(:all)
+      exit 9999
+    end
+  end
 end
